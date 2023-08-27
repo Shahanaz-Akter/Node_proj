cd0c3079241a95e5fb4c9000dec65ab3ef093c0b
Shahanaz_Akter <shahanazakterm1999@gmail.com>
2023-08-27 17:21:56 +0600

    first commit

---
+++ myProject/home1.js
@@ -0,0 +1,13 @@
+const hello = () => {
+    console.log("hello function");
+}
+
+
+const fun1 = () => {
+    console.log("Fun1");
+}
+
+exports.fun2 = () => {
+    console.log("Fun2fghh");
+}
+
---
+++ myProject/index.js
@@ -0,0 +1,11 @@
+const getName = () => {
+    console.log("Shahanaz Akter");
+}
+
+getName();
+
+const { fun2 } = require('./home1');
+// ob.fun1();
+console.log(fun2());
+console.log(fun2());
+
